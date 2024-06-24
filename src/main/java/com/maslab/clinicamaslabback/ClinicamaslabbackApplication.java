66ee21b502345996ee960774d6e7c851316aa6c1
Bruno Martins <88806986+BrunoMartinsGameDev@users.noreply.github.com>
2024-06-24 08:48:06 -0300

    Initial commit

---
+++ src/main/java/com/maslab/clinicamaslabback/ClinicamaslabbackApplication.java
@@ -0,0 +1,13 @@
+package com.maslab.clinicamaslabback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicamaslabbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicamaslabbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maslab/clinicamaslabback/ClinicamaslabbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maslab.clinicamaslabback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicamaslabbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
